c7410dba07f4808107026746d0ed1e273da6fdf5
naveen-nm27 <naveenm2795@gmail.com>
2026-09-02 11:21:39 +0530

    feat: update package.json scripts and add concurrently for development
    - Added development scripts for client and server in the root package.json.
    - Introduced a concurrent script to run both client and server development processes.
    - Added concurrently as a devDependency.
    
    refactor: rename shared packages to include scope
    
    - Updated package names for shared-types and shared-validation to include the @universe scope.

--- apps/server/src/server.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-
-const app = express()
-app.use(express.json());
-
-app.get("/health",(_req,res) => res.json({statu:"ok"}));
-
-const PORT = process.env.PORT || 4000;
-app.listen(PORT,()=> console.log(`Server is Running on ${PORT}`));
